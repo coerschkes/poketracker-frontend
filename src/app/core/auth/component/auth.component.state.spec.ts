1e8e3255c17ae1c594370b1651e045e0490d1196
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-02 00:37:51 +0200

    test: Implement tests for auth component state.

--- src/app/core/auth/component/auth.component.state.spec.ts
+++ src/app/core/auth/component/auth.component.state.spec.ts
@@ -1,1 +1,45 @@
-//todo: implement basic tests
+import {AuthComponentState} from "./auth.component.state";
+
+describe("AuthComponentState", () => {
+  let state: AuthComponentState;
+
+  beforeEach(async () => {
+    state = new AuthComponentState()
+  })
+
+  it('should be created', () => {
+    expect(state).toBeTruthy();
+  });
+
+  it('should initialize with isLoading() false', () => {
+    expect(state.isLoading()).toBeFalse()
+  });
+
+  it('should initialize with isLoginMode() false', () => {
+    expect(state.isLoginMode()).toBeFalse()
+  });
+
+  it('should revert isLoading on toggleLoading()', () => {
+    state.toggleLoading()
+    expect(state.isLoading()).toBeTrue()
+  });
+
+  it('should revert isLoginMode on toggleLoginMode()', () => {
+    state.toggleLoginMode()
+    expect(state.isLoginMode()).toBeTrue()
+  });
+
+  it('should revert isLoading and revert it back on toggleLoading() two times', () => {
+    state.toggleLoading()
+    state.toggleLoading()
+    expect(state.isLoading()).toBeFalse()
+  });
+
+  it('should reset to initial state on reset()', () => {
+    state.toggleLoading()
+    state.toggleLoginMode()
+    state.reset()
+    expect(state.isLoading()).toBeFalse()
+    expect(state.isLoginMode()).toBeFalse()
+  });
+})
