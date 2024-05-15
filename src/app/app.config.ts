87741e1f7557d2d6eff26d9480940050a0511c2a
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-15 16:41:59 +0200

    feat: Disable SSR and hydration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,10 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
