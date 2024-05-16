b857fd583b8eed3d5bcb153eac08d47a9aa43d7c
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-16 08:43:25 +0200

    feat: Add lazy loading.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import {Routes} from '@angular/router';
-import {AuthComponent} from "./core/auth/component/auth.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
 import {LoginGuard} from "./core/auth/login.guard";
 
 export const routes: Routes = [
-  {path: 'auth', component: AuthComponent},
-  {path: 'dashboard', component: DashboardComponent, canActivate: [LoginGuard.canActivate]},
-  {path: '', component: DashboardComponent, canActivate: [LoginGuard.canActivate]},
+  {
+    path: 'auth',
+    loadComponent: () => import('./core/auth/component/auth.component').then(m => m.AuthComponent)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [LoginGuard.canActivate]
+  },
+  // todo: redirect to error page here
   {path: '**', redirectTo: 'dashboard'}
 ];
-//todo: add page not found component and add back to root button / redirect in ... seconds to root
-//todo: test routing
