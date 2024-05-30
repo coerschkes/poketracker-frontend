78a39aef372bc13b1b9d9aa70d9b90af6d2872de
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-30 22:35:31 +0200

    docs: Add a todo.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -35,6 +35,7 @@ import {PokemonTypeComponent} from "../shared/pokemon-type/pokemon-type.componen
 //todo: add a search bar to filter the pokemon
 //todo: add buttons for deleting, editing and adding pokemon
 //todo: add statistics? (total pokemon, total shiny, total normal, total universal, total regional), piechart?
+//todo: pagination?
 export class DashboardComponent implements OnInit {
   private _poketrackerApi: any;
   protected _dataSource: Pokemon[];
