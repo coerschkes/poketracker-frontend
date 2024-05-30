bd53ad51f58bb29068444c840e94262cf5345858
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-05-30 22:27:00 +0200

    feat: Show a table of all pokemon on the dashboard.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,68 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SidenavComponent} from "../sidenav/sidenav.component";
+import {PoketrackerApiService} from "../core/external/poketracker/poketracker-api.service";
+import {Pokemon} from "../core/external/poketracker/poketracker-api";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {TitleCasePipe, UpperCasePipe} from "@angular/common";
+import {PokemonTypeComponent} from "../shared/pokemon-type/pokemon-type.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    SidenavComponent
+    SidenavComponent,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    UpperCasePipe,
+    TitleCasePipe,
+    PokemonTypeComponent
+  ],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private _poketrackerApi: any;
+  protected _dataSource: Pokemon[];
 
+  constructor(_poketrackerApi: PoketrackerApiService) {
+    this._poketrackerApi = _poketrackerApi;
+    this._dataSource = [];
+  }
+
+  ngOnInit(): void {
+    this._poketrackerApi.getAllPokemon().subscribe({
+      next: (value: Pokemon[]) => {
+        this._dataSource = value;
+      },
+      error: (e: HttpErrorResponse) => {
+        console.log(e);
+      }
+    });
+  }
+  //   dex: number
+  //   name: string
+  //   types: string[]
+  //   shiny: boolean
+  //   normal: boolean
+  //   universal: boolean
+  //   regional: boolean
+  //   editions: string[]
+  //   normalSpriteUrl: string
+  //   shinySpriteUrl: string
+
+  columnsToDisplay = ['dex', 'name', 'types', 'shiny', 'normal', 'universal', 'regional'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement: Pokemon | null;
 }
